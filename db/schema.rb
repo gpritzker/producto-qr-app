461e3c49bcf8406709a4b62d09d2263b342807bf
Gonzalo Pritzker <gonzalo.pritzker@avenida.com>
2024-11-03 15:05:36 -0300

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,18 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_11_01_185728) do
-  create_table "products", charset: "utf8mb3", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.decimal "price", precision: 10
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
