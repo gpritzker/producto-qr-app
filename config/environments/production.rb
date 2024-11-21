f216113a54ae6f375302b080c3a3ac3215405590
Paganini Pablo <pablo.paganini@gmail.com>
2024-11-21 21:28:21 +0000

    config.assets.enabled = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
-  config.assets.enabled = false
+  config.assets.enabled = true
 
   config.active_storage.service = :local
 
