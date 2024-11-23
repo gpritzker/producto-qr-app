ca021b33681c7599589cf4967c344953bba11e2e
Gonzalo Pritzker <gonzalo.pritzker@avenida.com>
2024-11-22 21:01:33 -0300

    agrego mail de qarapp

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'gjpritzker@gmail.com'
+  config.mailer_sender = 'qarapparapp@gmail.com'
   
   #config.default_locale = :es
   # Configure the class responsible to send e-mails.
