c1a09033c3a5487dc398e02cd4feb1a47231bbb5
Paganini Pablo <pablo.paganini@gmail.com>
2024-12-11 22:26:02 +0000

    feat: pdf para djc

--- app/models/djc.rb
+++ app/models/djc.rb
@@ -47,17 +47,15 @@ class Djc < ApplicationRecord
   validate :technical_normatives_must_be_an_array #[normativa, ...]
 
   before_validation :normalize_attributes
-  
-  
-  has_paper_trail on: [:create, :update], save_changes: true
-
-  after_save :track_file_changes, if: :attachments_changed?
-  
-
   before_save :set_attachment_flags
-
+  after_save :track_file_changes, if: :attachments_changed?
+    
+  has_paper_trail on: [:create, :update], save_changes: true
+  
   attr_accessor :djc_file_changed, :crs_files_changed
   attr_accessor :skip_track_file_changes
+
+  DJC_MAX_PRODUCT_ATTRIBUTES_IN_PDF = 4
 
   def set_attachment_flags
     self.djc_file_changed = djc_file.attached? && !djc_file.blob.nil?
@@ -67,95 +65,105 @@ class Djc < ApplicationRecord
   def attachments_changed?
     crs_files.attached?
   end
-  #after_save :generate_pdf
-
-  # def generate_pdf
-  #   nombre = "djc-#{qr.code}-#{id}"
-  #   # Generar el PDF como un string binario
-  #   controller = ActionController::Base.new
-  #   pdf_content = controller.render_to_string(
-  #     pdf: nombre, # Nombre del archivo
-  #     template: "djcs/show",       # Vista para el PDF
-  #     encoding: "UTF-8",
-  #     margin: { top: 10, bottom: 10, left: 10, right: 10 },
-  #     locals: { djc: self },
-  #     page_size: "A4" # Usa un tamaño de página estándar
-  #   )
-    
-  #   # Definir el path donde se guardará el archivo
-  #   if signed_by.present?
-  #     file_path = Rails.root.join("public", "pdfs", "#{nombre}.pdf")
-  #   else  
-  #     file_path = Rails.root.join("public", "pdfs", "#{nombre}-tmp.pdf")
-  #   end  
-
-  #   # Crear el directorio si no existe
-  #   FileUtils.mkdir_p(File.dirname(file_path))
-
-  #   # Guardar el contenido del PDF en un archivo
-  #   File.open(file_path, 'ab') do |file|
-  #     file.write(pdf_content)
-  #   end
-
-  #   # Agrego la marca de agua
-  #   unless signed_by.present?
-  #     watermark_pdf_path = Rails.root.join("app", "assets", "watermark.pdf")
-  #     main_pdf_path = Rails.root.join("public", "pdfs", "#{nombre}-tmp.pdf")
-  #     output_pdf_path = Rails.root.join("public", "pdfs", "#{nombre}.pdf")
-  #     system("rm #{output_pdf_path}")
-  #     system("hexapdf watermark -w #{watermark_pdf_path} -t stamp #{main_pdf_path} #{output_pdf_path}")
-  #     system("rm #{main_pdf_path}")
-  #   end    
-  # end
-
+  
   def generate_pdf
     nombre = "djc-#{qr.code}-#{id}"
+    pdf_base_path = Rails.root.join("app", "assets", "base_djc.pdf")
+    pdf_path = Rails.root.join("tmp", "#{nombre}.pdf")
+
+    temporal_static_path = Rails.root.join("tmp", "#{nombre}-static.pdf")
+    temporal_base_path = Rails.root.join("tmp", "#{nombre}-base.pdf")
+    temporal_body_path = Rails.root.join("tmp", "#{nombre}-body.pdf")
+    temporal_base_body_path = Rails.root.join("tmp", "#{nombre}-base-body.pdf")
+    temporal_anexo_path = Rails.root.join("tmp", "#{nombre}-anexo.pdf")
+    temporal_anexo_base_path = Rails.root.join("tmp", "#{nombre}-base-anexo.pdf")
     
-    # Generar el PDF como un string binario
+    # Generar el PDF base temporal (con datos estáticos como QR, Firma, Nombres y Titulo)
     controller = ActionController::Base.new
     pdf_content = controller.render_to_string(
-      pdf: nombre,               # Nombre del archivo
-      template: "djcs/show",      # Vista para el PDF
+      pdf: nombre,
+      template: "djcs/pdf_base",
       encoding: "UTF-8",
       margin: { top: 10, bottom: 10, left: 10, right: 10 },
       locals: { djc: self },
-      page_size: "A4"            # Usa un tamaño de página estándar
+      page_size: "A4"
     )
-  
-    # Definir el nombre del archivo
-    file_name = "#{nombre}.pdf"
-  
-    # Agrego la marca de agua si es necesario
-    unless signed_by.present?
-      watermark_pdf_path = Rails.root.join("app", "assets", "watermark.pdf")
-      main_pdf_path = Rails.root.join("tmp", "#{file_name}-tmp.pdf")
-      output_pdf_path = Rails.root.join("tmp", file_name)
-  
-      # Guardar el contenido en un archivo temporal
-      File.open(main_pdf_path, 'wb') do |file|
-        file.write(pdf_content)
-      end
-  
-      # Aplicar marca de agua
-      system("hexapdf watermark -w #{watermark_pdf_path} -t stamp #{main_pdf_path} #{output_pdf_path}")
-      system("rm #{main_pdf_path}")
-    else
-      # Si ya está firmado, solo guardamos el PDF sin marca de agua
-      output_pdf_path = Rails.root.join("tmp", file_name)
-      File.open(output_pdf_path, 'wb') do |file|
-        file.write(pdf_content)
-      end
+    File.open(temporal_static_path, "wb") do |file|
+      file.write(pdf_content)
     end
+    
+    # Generar el PDF Body, con los datos de la DJC
+    pdf_content = controller.render_to_string(
+      pdf: nombre,
+      template: "djcs/pdf_body",
+      encoding: "UTF-8",
+      margin: { top: 45, bottom: 65, left: 15, right: 10 },
+      locals: { djc: self },
+      page_size: "A4"
+    )
+    File.open(temporal_body_path, "wb") do |file|
+      file.write(pdf_content)
+    end
+    
+    # Combino los pdf
+    if system("hexapdf watermark -w #{pdf_base_path} -i 1 -t background #{temporal_static_path} #{temporal_base_path}")
+      system("hexapdf watermark -w #{temporal_base_path} -i 1 -t background #{temporal_body_path} #{temporal_base_body_path}")
+    end
+    
+    # Si tiene mas de DJC_MAX_PRODUCT_ATTRIBUTES_IN_PDF caracteristicas se crea un anexo
+    if product_attributes.size > DJC_MAX_PRODUCT_ATTRIBUTES_IN_PDF
+      pdf_content = controller.render_to_string(
+        pdf: nombre,
+        template: "djcs/pdf_anexo",
+        encoding: "UTF-8",
+        margin: { top: 45, bottom: 65, left: 15, right: 10 },
+        locals: { djc: self },
+        page_size: "A4"
+      )
+      File.open(temporal_anexo_path, "wb") do |file|
+        file.write(pdf_content)
+      end
+      if system("hexapdf watermark -w #{temporal_base_path} -i 1 -t background #{temporal_anexo_path} #{temporal_anexo_base_path}")
+        system("hexapdf merge #{temporal_base_body_path} #{temporal_anexo_base_path} #{pdf_path}")
+      end
+    else
+      system("mv #{temporal_base_body_path} #{pdf_path}")
+    end
+    # Agrego la marca de agua si es necesario
+    # unless signed_by.present?
+    #   watermark_pdf_path = Rails.root.join("app", "assets", "watermark.pdf")
+    #   main_pdf_path = Rails.root.join("tmp", "#{file_name}-tmp.pdf")
+    #   output_pdf_path = Rails.root.join("tmp", file_name)
+  
+    #   # Guardar el contenido en un archivo temporal
+    #   File.open(main_pdf_path, 'wb') do |file|
+    #     file.write(pdf_content)
+    #   end
+  
+    #   # Aplicar marca de agua
+    #   system("hexapdf watermark -w #{watermark_pdf_path} -t stamp #{main_pdf_path} #{output_pdf_path}")
+    #   system("rm #{main_pdf_path}")
+    # else
+    #   # Si ya está firmado, solo guardamos el PDF sin marca de agua
+    #   output_pdf_path = Rails.root.join("tmp", file_name)
+    #   File.open(output_pdf_path, 'wb') do |file|
+    #     file.write(pdf_content)
+    #   end
+    # end
   
     # Subir el archivo PDF a ActiveStorage (Amazon S3)
-    file = File.open(output_pdf_path)
-  
-    # Crear el attachment en ActiveStorage
-    self.djc_file.attach(io: file, filename: file_name, content_type: 'application/pdf')
-  
-    # Eliminar el archivo temporal después de cargarlo a S3
+    file = File.open(pdf_path)
+    self.djc_file.attach(io: file, filename: nombre, content_type: 'application/pdf')
     file.close
-    File.delete(output_pdf_path) if File.exist?(output_pdf_path)
+
+    # Borro temporales
+    File.delete(temporal_static_path) if File.exist?(temporal_static_path)
+    File.delete(temporal_base_path) if File.exist?(temporal_base_path)
+    File.delete(temporal_body_path) if File.exist?(temporal_body_path)
+    File.delete(temporal_base_body_path) if File.exist?(temporal_base_body_path)
+    File.delete(temporal_anexo_path) if File.exist?(temporal_anexo_path)
+    File.delete(temporal_anexo_base_path) if File.exist?(temporal_anexo_base_path)
+    File.delete(pdf_path) if File.exist?(pdf_path)
   end 
 
   def can_approve?
@@ -223,47 +231,6 @@ class Djc < ApplicationRecord
       end
     end
   end
-  
-
-  # def track_file_changes
-  #   return unless attachments_changed?
-  
-  #   custom_changes = {}
-  
-  #   # Cambios en `djc_file`
-  #   if djc_file.attached?
-  #     custom_changes[:djc_file] = {
-  #       status: "Archivo adjunto",
-  #       filename: djc_file.filename.to_s,
-  #       content_type: djc_file.content_type,
-  #       byte_size: djc_file.byte_size
-  #     }
-  #   else
-  #     custom_changes[:djc_file] = { status: "Archivo eliminado" }
-  #   end
-  
-  #   # Cambios en `crs_files`
-  #   if crs_files.attached?
-  #     latest_file = crs_files.last
-  #     custom_changes[:crs_files] = {
-  #       filename: latest_file.filename.to_s,
-  #       content_type: latest_file.content_type,
-  #       byte_size: latest_file.byte_size
-  #     }
-  #   else
-  #     custom_changes[:crs_files] = { status: "Archivos eliminados" }
-  #   end
-  
-  #   # Guarda los cambios personalizados en `object_changes`
-  #   if custom_changes.any?
-  #     latest_version = versions.last
-  #     if latest_version
-  #       existing_changes = latest_version.object_changes.present? ? YAML.safe_load(latest_version.object_changes, permitted_classes: [ActiveSupport::TimeWithZone], aliases: true) : {}
-  #       latest_version.update_columns(object_changes: existing_changes.merge(custom_changes).to_json)
-  #     end
-  #   end
-  # end
-   
    
   def product_attributes_must_be_an_array_of_hashes
     unless product_attributes.is_a?(Array) && product_attributes.size.positive?
--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,9 +1,11 @@
 if Rails.env.production?
   WickedPdf.config = {
-    exe_path: '/app/bin/wkhtmltopdf'
+    exe_path: '/app/bin/wkhtmltopdf',
+    enable_local_file_access: true
   }
 else
   WickedPdf.config ||= {
-    exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+    exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf'),
+    enable_local_file_access: true
   }
 end
