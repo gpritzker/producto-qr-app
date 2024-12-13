a79f6116947925a5134094f19431ffdf7765f59e
Paganini Pablo <pablo.paganini@gmail.com>
2024-12-13 22:40:44 +0000

    fix: secret key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,9 @@ class ApplicationController < ActionController::API
 
   def authenticate_user_from_token!
     @auth_token = request.headers['Authorization'].to_s.split(' ').last
+    secret_key = "ePFj4GrQiJdVzyzgdFJiCKWEVWe+UT1mMZGzUM+kqSpZJf2nnecwljT15dTC1EWlJs9iDAHEfPWZlRohm0hruXDtR+Npl9x0Izjj5KqVAhE4sikD/cBmbgex7u8hAg/ruY+xtziRMutdNXOqioXCE2hOe0xTGcvIjL7rUcoSle+Q3ssEu/q9Q05eDnOxC76axpEgBlmDInLtQPVA+f2RBK3DdJ4Pa11Euv9R6wKj5IPl8rsI03K1ITPK6yuwkQZVdQjyfzlbfVmy/w+4nNooDs1KNfcaUqG/OOLg36oyfeIVnQn5IV5OunCFhlrubQEpviuxgBle3hP8FnbtFbqGVj0lfpYtrV+a/3axXnRWZhh7niX5Vx66fdD+cr6+DJ8CL6ENAwtSsD4XEcKcfDEG1fl8Hb95/omYQwbI--wo5CrzwMW95rZ2JD--CajgSL4Jk76CsgI509JiEw=="
     begin
-      decoded_token = JWT.decode(@auth_token, Rails.application.secret_key_base, true, { algorithm: 'HS256', leeway: 10 })
+      decoded_token = JWT.decode(@auth_token, secret_key, true, { algorithm: 'HS256', leeway: 10 })
       user_id = decoded_token[0]['user_id']
       @current_user = User.find(user_id)
     rescue JWT::ExpiredSignature => e
